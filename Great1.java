d50833c4cc74076f9be56e2f2c2d1fa828732508
keerthana1808 <40415808+keerthana1808@users.noreply.github.com>
2018-06-22 15:56:27 +0530

    Update and rename Great.java to Great1.java

---
+++ Great1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Great1
+{
+    public static void main(String args[])
+    {
+      Scanner sc=new Scanner(System.in);
+      String s=sc.nextLine();
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println(s);
+        }
+    }
+}
